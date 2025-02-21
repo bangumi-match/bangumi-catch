5770c65f9555a4a73084f4773b5e755b1864f7f5
Lambert Rao <101875596+Lambert-Rao@users.noreply.github.com>
2025-02-21 09:41:33 +0800

    fix: don't save empty user file

--- tools/deleteEmpty.py
+++ tools/deleteEmpty.py
@@ -9,5 +9,14 @@ def delete_empty_json_files(directory):
           os.remove(file_path)
           print(f"Deleted: {file_path}")
 
+def delete_empty_dirs(directory):
+  for root, dirs, _ in os.walk(directory, topdown=False):
+    for dir in dirs:
+      dir_path = os.path.join(root, dir)
+      if not os.listdir(dir_path):
+        os.rmdir(dir_path)
+        print(f"Deleted empty directory: {dir_path}")
+
 if __name__ == "__main__":
   delete_empty_json_files("../user_data")
+  delete_empty_dirs("../user_data")
